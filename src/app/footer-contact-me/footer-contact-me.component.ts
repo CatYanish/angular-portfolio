f61d667ff4887c389cb84c13969341652a6f53e6
Cat Yanish <catyanish@gmail.com>
2018-01-01 16:08:10 -0600

    footer looks excellent

--- src/app/footer-contact-me/footer-contact-me.component.ts
+++ src/app/footer-contact-me/footer-contact-me.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-contact-me',
   template: `
-    <p>
+    <p fxLayout.xs="column" fxLayoutAlign="center center" fxLayoutGap="10px">
       <a href="https://www.linkedin.com/in/catherine-yanish-64a7a264/">LinkedIn</a>
-      <a href="https://github.com/CatYanish" fxLayoutAlign.xs="center center">GitHub</a>
-      <a href="mailto:catherineyanish@gmail.com" fxLayoutAlign.xs="center center" class="emailContact">catherineyanish@gmail.com</a>
+      <a href="https://github.com/CatYanish">GitHub</a>
+      <a href="mailto:catherineyanish@gmail.com" class="emailContact">catherineyanish@gmail.com</a>
     </p>
   `,
   styleUrls: ['./footer-contact-me.component.scss']
