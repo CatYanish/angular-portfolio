f6d7233b4abdf27a7a6f4279b2bd7d9350e060ab
Cat Yanish <catyanish@gmail.com>
2018-01-02 20:02:58 -0600

    my projects is layed out and styled

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -7,15 +7,26 @@ import { Component, OnInit } from '@angular/core';
       <h1 fxLayoutAlign="center center">
         My Projects
       </h1>
-      <div fxLayout="row">
-        <img src="../assets/GroupProjectPhoto.png"/>
-        <p>
-          Mobility For All
+      <div fxLayoutAlign="center center" fxLayout="row" fxLayout.xs="column">
+        <img fxLayout="column" src="../assets/GroupProjectPhoto.png"/>
+        <p fxLayout="column" fxFlex="50%" fxFlexOffset="5">
+          Mobility For All is like "Uber for People with Disabilities".
+          Mobility For All is a full-stack mobile-friendly web application, developed
+          as a prototype by me and my teammates at Prime Digital Academy.
+          We used a variety of Google Maps APIs, socket.io, and JavaScipt and PostgreSQL
+          to bring the idea to life in a three week sprint.
         </p>
       </div>
     </mat-card>
     <mat-card>
-      <img src="../assets/SoloProjectPhoto.png"/>
+      <div fxLayoutAlign="center center" fxLayout="row" fxLayout.xs="column">
+        <p fxLayout="column" fxFlex="50%">
+        HouseFull is a MEAN stack application I built with the idea of helping roommtes get along.
+        It features a happy color pallete and a simple mobile-responsive interface where housemates
+        can share notes and post house chore info.
+        </p>
+        <img fxLayout="column" fxFlexOffset="5" src="../assets/SoloProjectPhoto.png"/>
+      </div>
     </mat-card>
   `,
   styleUrls: ['./my-projects.component.scss']
