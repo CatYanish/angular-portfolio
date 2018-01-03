c5169ee23622ef84bf6d1892d6e5a86fa10cb0e7
Cat Yanish <catyanish@gmail.com>
2018-01-02 19:33:26 -0600

    styling my projects section

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-projects',
   template: `
-    <p>
-      my-projects works!
-    </p>
-    <img src="../assets/GroupProjectPhoto.png"/>
-    <img src="../assets/SoloProjectPhoto.png"/>
+    <mat-card>
+      <h1 fxLayoutAlign="center center">
+        My Projects
+      </h1>
+      <div fxLayout="row">
+        <img src="../assets/GroupProjectPhoto.png"/>
+        <p>
+          Mobility For All
+        </p>
+      </div>
+    </mat-card>
+    <mat-card>
+      <img src="../assets/SoloProjectPhoto.png"/>
+    </mat-card>
   `,
   styleUrls: ['./my-projects.component.scss']
 })
